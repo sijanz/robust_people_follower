1b4208291780afbc6343bd499d44d0fd42749972
sijanz <simon.janzon@gmx.net>
2019-05-03 00:25:17 +0200

    implemented a callback function for odometry data, general code cleanup

--- src/robust_people_follower.cpp
+++ src/robust_people_follower.cpp
@@ -1,7 +1,9 @@
+#include <stdlib.h>
+
 #include <ros/ros.h>
 #include <body_tracker_msgs/Skeleton.h>
 #include <geometry_msgs/Twist.h>
-#include <stdlib.h>
+#include <nav_msgs/Odometry.h>
 
 
 // FIXME: now that's ugly (supports only one person anyway). DEPRECATED - substitute with new data structure!
@@ -11,7 +13,7 @@ float distanceCenterOfMass;
 float centerOfMassY;
 
 
-// ### DATA STRUCTURES ###
+// ####### DATA STRUCTURES #######
 
 /**
  * Stores a position in 2-dimensional space.
@@ -24,12 +26,11 @@ struct Position2D
 
 
 /**
- * Store a pose in 2-dimensional space.
+ * Stores an orientation in 2-dimensional space.
  */
-struct Pose
+struct Orientation
 {
-    double x;
-    double y;
+
     double z;
     double w;
 };
@@ -54,13 +55,13 @@ class Turtlebot
 {
 public:
     Position2D position{};
-    Pose pose{};
+    Orientation orientation{};
 
 
     Turtlebot()
     {
         position.x = position.y = 0;
-        pose.x = pose.y = pose.z = pose.w = 0;
+        orientation.z = orientation.w = 0;
     }
 };
 
@@ -73,7 +74,6 @@ class Person
 public:
     SkeletonInfo skeleton{};
     Position2D position{};
-    Pose pose{};
     double velocity;
 
 
@@ -82,59 +82,52 @@ public:
         skeleton.bodyId = skeleton.numberOfGestures = 0;
         skeleton.centerOfMassY = skeleton.distanceCenterOfMass = 0;
         position.x = position.y = 0;
-        pose.x = pose.y = pose.z = pose.w = 0;
         velocity = 0;
     }
 };
 
 
-void debugPrintout(const Turtlebot &turtlebot, const std::list<Person> &trackedPersons, const Person &target);
+// ####### FUNCTION PROTOTYPES #######
 
 
-/**
- * @brief Sets the global variables to values in the message from the subscribed topic.
- * @param msg the message from the subscribed topic
- */
-void skeletonCallback(const body_tracker_msgs::Skeleton::ConstPtr &msg)
-{
+void debugPrintout();
 
-    // TODO: select new ID when gesture is done for at least 3 seconds
-    if (msg->gesture == 2) {
-        bodyId = msg->body_id;
+void odometryCallback(const nav_msgs::Odometry::ConstPtr &msg);
 
-        // DEBUG
-        ROS_INFO("bodyId to track: %d", bodyId);
-    }
+void skeletonCallback(const body_tracker_msgs::Skeleton::ConstPtr &msg);
 
-    if (msg->body_id == bodyId) {
-        numberOfGestures = msg->gesture;
-        distanceCenterOfMass = msg->centerOfMass.x;
-        centerOfMassY = msg->centerOfMass.y;
-    }
-}
+
+// ####### GLOBAL VARIABLES #######
+
+
+// instance of the turtlebot robot
+Turtlebot turtlebot;
+
+// list of persons in the frame
+std::vector<Person> trackedPersons;
+
+// instance of target
+Person target;
+
+
+// ####### ENTRY POINT #######
 
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "body_tracker");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("body_tracker/skeleton", 10, skeletonCallback);
+
+    ros::Subscriber odomSub = n.subscribe("/odom", 10, odometryCallback);
+    ros::Subscriber skeletonSub = n.subscribe("/body_tracker/skeleton", 10, skeletonCallback);
+
     ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 1000);
 
     ros::Rate loop_rate(10);
 
-    // instance of the turtlebot robot
-    Turtlebot turtlebot;
-
-    // list of persons in the frame
-    std::list<Person> trackedPersons;
-
-    // instance of target
-    Person target;
-
     while (ros::ok()) {
 
-        debugPrintout(turtlebot, trackedPersons, target);
+        debugPrintout();
 
         // message to store velocity commands in
         geometry_msgs::Twist msg;
@@ -166,32 +159,66 @@ int main(int argc, char **argv)
 }
 
 
-void debugPrintout(const Turtlebot &turtlebot, const std::list<Person> &trackedPersons, const Person &target)
+void debugPrintout()
 {
     system("clear");
 
     ROS_INFO("Turtlebot info:");
-    ROS_INFO("position:");
-    ROS_INFO("  x: %f", turtlebot.position.x);
-    ROS_INFO("  y: %f", turtlebot.position.y);
-    ROS_INFO("pose:");
-    ROS_INFO("  x: %f", turtlebot.pose.x);
-    ROS_INFO("  y: %f", turtlebot.pose.y);
-    ROS_INFO("  z: %f", turtlebot.pose.z);
-    ROS_INFO("  w: %f\n", turtlebot.pose.w);
+    ROS_INFO("  position:");
+    ROS_INFO("    x: %f", turtlebot.position.x);
+    ROS_INFO("    y: %f", turtlebot.position.y);
+    ROS_INFO("  orientation:");
+    ROS_INFO("    z: %f", turtlebot.orientation.z);
+    ROS_INFO("    w: %f\n", turtlebot.orientation.w);
 
-    ROS_INFO("tracked persons: %lu", trackedPersons.size());
+    ROS_INFO("tracked persons: %lu\n", trackedPersons.size());
     if (!trackedPersons.empty()) {
-        for (Person p : trackedPersons) {
-            ROS_INFO("id: %d", p.skeleton.bodyId);
-            ROS_INFO("position:");
-            ROS_INFO("  x: %f", p.position.x);
-            ROS_INFO("  y: %f", p.position.y);
-            ROS_INFO("number of gestures: %d", p.skeleton.numberOfGestures);
-            ROS_INFO("distance: %f", p.skeleton.distanceCenterOfMass);
-            ROS_INFO("y-deviation of center of mass: %f\n", p.skeleton.centerOfMassY);
+        for (std::vector<Person>::iterator iter = trackedPersons.begin(); iter != trackedPersons.end(); ++iter) {
+            ROS_INFO("id: %d", iter->skeleton.bodyId);
+            ROS_INFO("  position (relative):");
+            ROS_INFO("    x: %f", iter->position.x);
+            ROS_INFO("    y: %f", iter->position.y);
+            ROS_INFO("  position (absolute):");
+            ROS_INFO("    x: %f", iter->position.x);
+            ROS_INFO("    y: %f", iter->position.y);
+            ROS_INFO("  number of gestures: %d", iter->skeleton.numberOfGestures);
+            ROS_INFO("  distance: %f", iter->skeleton.distanceCenterOfMass);
+            ROS_INFO("  y-deviation of center of mass: %f\n", iter->skeleton.centerOfMassY);
         }
     }
 
-    ROS_INFO("target: %d", target.skeleton.bodyId);
+    ROS_INFO("target id: %d", target.skeleton.bodyId);
+}
+
+
+void odometryCallback(const nav_msgs::Odometry::ConstPtr &msg)
+{
+    turtlebot.position.x = msg->pose.pose.position.x;
+    turtlebot.position.y = msg->pose.pose.position.y;
+    turtlebot.orientation.z = msg->pose.pose.orientation.z;
+    turtlebot.orientation.w = msg->pose.pose.orientation.w;
+}
+
+
+/**
+ * @brief Sets the global variables to values in the message from the subscribed topic.
+ * @param msg the message from the subscribed topic
+ */
+void skeletonCallback(const body_tracker_msgs::Skeleton::ConstPtr &msg)
+{
+
+    // TODO: select new ID when gesture is done for at least 3 seconds
+    // TODO: play sound
+    if (msg->gesture == 2) {
+        bodyId = msg->body_id;
+
+        // DEBUG
+        ROS_INFO("bodyId to track: %d", bodyId);
+    }
+
+    if (msg->body_id == bodyId) {
+        numberOfGestures = msg->gesture;
+        distanceCenterOfMass = msg->centerOfMass.x;
+        centerOfMassY = msg->centerOfMass.y;
+    }
 }
