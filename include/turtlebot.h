06820a0566abf72d7ef02ee1c652c5a224081fa7
sijanz <simon.janzon@gmx.net>
2019-05-13 17:46:00 +0200

    added license comment

--- include/turtlebot.h
+++ include/turtlebot.h
@@ -1,3 +1,38 @@
+/*********************************************************************
+* Software License Agreement (BSD 3-Clause License)
+*
+*  Copyright (c) 2019, Simon Janzon
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*  1. Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*  2. Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*  3. Neither the name of the copyright holder nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+
 #ifndef ROBUST_PEOPLE_FOLLOWER_TURTLEBOT_H
 #define ROBUST_PEOPLE_FOLLOWER_TURTLEBOT_H
 
@@ -197,57 +232,57 @@ geometry_msgs::Twist& Turtlebot::setVelocityCommand(const Person& t_target,
          */
 
         //if (t_target.getDistance() > 1800) {
-            if (distance_to_goal < 0.3) {
+        if (distance_to_goal < 0.3) {
 
-                // input 40% less acceleration
-                t_msg.linear.x = m_current_linear - (m_current_linear / 100) * 40;
-                t_msg.angular.z = m_current_angular - (m_current_angular / 100) * 40;
+            // input 40% less acceleration
+            t_msg.linear.x = m_current_linear - (m_current_linear / 100) * 40;
+            t_msg.angular.z = m_current_angular - (m_current_angular / 100) * 40;
 
-                t_goal_list.pop_front();
+            t_goal_list.pop_front();
 
-                if (!t_goal_list.empty())
-                    current_goal = t_goal_list.at(0);
+            if (!t_goal_list.empty())
+                current_goal = t_goal_list.at(0);
 
 
-            } else if (angle_to_goal - m_angle > 0.2)
-                t_msg.angular.z = 0.5 * (angle_to_goal - m_angle);
-            else if (angle_to_goal - m_angle < -0.2)
-                t_msg.angular.z = -0.5 * std::abs(angle_to_goal - m_angle);
-            else
-                t_msg.linear.x = 0.4;
-                //t_msg.linear.x = 0.32 * (t_target.getDistance() / 1000) - 0.576;
+        } else if (angle_to_goal - m_angle > 0.2)
+            t_msg.angular.z = 0.5 * (angle_to_goal - m_angle);
+        else if (angle_to_goal - m_angle < -0.2)
+            t_msg.angular.z = -0.5 * std::abs(angle_to_goal - m_angle);
+        else
+            t_msg.linear.x = 0.4;
+        //t_msg.linear.x = 0.32 * (t_target.getDistance() / 1000) - 0.576;
 
-            // move backwards if target is too near
-        } else if (t_target.getDistance() < 1000 && t_target.getDistance() > 0) {
-            t_msg.linear.x = 2 * (t_target.getDistance() / 1000) - 2;
-        }
-
-            /*// keep human in center if the distance is less than the threshold
-        else {
-            if (t_target.getYDeviation() < -50)
-                t_msg.angular.z = -0.0025 * std::abs(t_target.getYDeviation());
-            else if (t_target.getYDeviation() > 50)
-                t_msg.angular.z = 0.0025 * t_target.getYDeviation();
-        }*/
-        m_current_linear = t_msg.linear.x;
-        m_current_angular = t_msg.angular.z;
-
-        /*
-        // no goals
-    } else {
-        // angular velocity
-        if (t_target.getYDeviation() < -50) {
-            t_msg.angular.z = -0.0025 * std::abs(t_target.getYDeviation());
-        } else if (t_target.getYDeviation() > 50) {
-            t_msg.angular.z = 0.0025 * t_target.getYDeviation();
-        } else
-            t_msg.angular.z = 0;
-
-        // linear velocity
-        if (t_target.getDistance() < 1000 && t_target.getDistance() > 0.0) {
-            t_msg.linear.x = 2 * (t_target.getDistance() / 1000) - 2;
-        }
+        // move backwards if target is too near
+    } else if (t_target.getDistance() < 1000 && t_target.getDistance() > 0) {
+        t_msg.linear.x = 2 * (t_target.getDistance() / 1000) - 2;
     }
+
+    /*// keep human in center if the distance is less than the threshold
+else {
+    if (t_target.getYDeviation() < -50)
+        t_msg.angular.z = -0.0025 * std::abs(t_target.getYDeviation());
+    else if (t_target.getYDeviation() > 50)
+        t_msg.angular.z = 0.0025 * t_target.getYDeviation();
+}*/
+    m_current_linear = t_msg.linear.x;
+    m_current_angular = t_msg.angular.z;
+
+    /*
+    // no goals
+} else {
+    // angular velocity
+    if (t_target.getYDeviation() < -50) {
+        t_msg.angular.z = -0.0025 * std::abs(t_target.getYDeviation());
+    } else if (t_target.getYDeviation() > 50) {
+        t_msg.angular.z = 0.0025 * t_target.getYDeviation();
+    } else
+        t_msg.angular.z = 0;
+
+    // linear velocity
+    if (t_target.getDistance() < 1000 && t_target.getDistance() > 0.0) {
+        t_msg.linear.x = 2 * (t_target.getDistance() / 1000) - 2;
+    }
+}
 */
 
     return t_msg;
