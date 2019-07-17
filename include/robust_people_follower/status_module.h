abb24c2aab6269623a8c0809abd4c6d5de5f54b9
sijanz <simon.janzon@gmx.net>
2019-07-17 15:13:37 +0200

    commenting

--- include/robust_people_follower/status_module.h
+++ include/robust_people_follower/status_module.h
@@ -39,34 +39,114 @@
 #include "object_2d_space.h"
 
 
+/**
+ * @brief The Status Module manages information about the current state of the robot. It holds the robot's status used
+ * to determine the robot's actions as well as its position, orientation and velocity. The Status Module receives its
+ * data from the /odom topic and provides its information to the other modules.
+ */
 class StatusModule : public Object2DSpace
 {
 public:
 
-    StatusModule() = default;
-
+    /**
+     * @brief Represents the status of the robot.
+     */
     enum Status : unsigned char
     {
-        WAITING = 0,
-        FOLLOWING = 1,
-        LOS_LOST = 2,
-        SEARCHING = 3
+
+        /** @brief The robots has a WAITING status if no target is selected */
+                WAITING = 0,
+
+        /** @brief Is set after the "follow-me"-gesture is recognized; the robot follows the target depending on its
+         * distance to the robot*/
+                FOLLOWING = 1,
+
+        /** @brief The robot lost the line of sight to the target but still has waypoints to drive to */
+                LOS_LOST = 2,
+
+        /** @brief The line of sight to the target is lost and no waypoints are left to drive to */
+                SEARCHING = 3
     };
 
-    // inherited methods
+
+    /*
+     * ********** SPECIAL METHODS **********
+     */
+
+    /**
+     * @brief Standard constructor. Initializes the status as well as the pose and velocity with their default values.
+     */
+    StatusModule() = default;
+
+
+    /*
+     * ********** INHERITED METHODS **********
+     */
+
+    /**
+     * @brief Calculates the angle of the robot by creating a quaternion out of the odometry data. The yaw value is then
+     * extracted from the quaternion and set as the robot's angle in the global reference system.
+     */
     void calculateAngle() override;
+
+
+    /**
+     * @brief Calculates the velocity of the robot by dividing the Euclidean distance between the current and the last
+     * position by the inverse of the frequency of the main loop.
+     *
+     * @param t_frequency the frequency of the main loop in Hz
+     */
     void calculateVelocity(double t_frequency) override;
+
+
+    /**
+     * @brief Prints out information about the robot. The information contains of the robot's status, velocity in m/s,
+     * position in the coordinate system as x-y-value and orientation in radians.
+     */
     void printInfo() const override;
 
-    // setters
+
+    /*
+     * ********** SETTERS **********
+     */
+
+    /**
+     * @brief Setter for the status of the robot.
+     *
+     * @return the status of the robot as a lvalue
+     */
     inline StatusModule::Status& status() { return m_status; }
 
-    // getters
+
+    /*
+     * ********** GETTERS **********
+     */
+
+    /**
+     * @brief Getter for th status of the robot.
+     *
+     * @return the status of the robot as a rvalue
+     */
     inline const StatusModule::Status status() const { return m_status; }
 
+
+    /*
+     * ********** STANDARD METHODS **********
+     */
+
+    /**
+     * @brief Sets the data extracted from the /odom topic to the members. It first sets the pose, before calculating
+     * the robot's angle and velocity. Lastly the robot's old pose is updated.
+     *
+     * @param t_pose pose information extracted from the /odom topic
+     * @param t_frequency frequency of the main loop to calculate the robot's velocity
+     */
     void processOdometryData(const geometry_msgs::Pose& t_pose, double t_frequency);
 
+
 private:
+
+    /** @brief Stores the current status of the robot in form of the Status enumeration */
     Status m_status{};
 };
 
